87c36f67d61d3a6a0668fa269eb5dae453afc80a
Shinzo SAITO <architshin@websarva.com>
2019-08-01 03:31:32 +0900

    index.phpの微修正。

--- firstslim/public/index.php
+++ firstslim/public/index.php
@@ -3,12 +3,13 @@ use Slim\Factory\AppFactory;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\ResponseInterface;
 
-require($_SERVER["DOCUMENT_ROOT"]."/firstslim/vendor/autoload.php");
+require_once($_SERVER["DOCUMENT_ROOT"]."/firstslim/vendor/autoload.php");
+
 $app = AppFactory::create();
 
 $app->get("/firstslim/public/hello",
-	function(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface{
-		print("Hello world!");
+	function(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface {
+		print("Hello World!");
 		return $response;
 	}
 );
