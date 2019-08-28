6ae02d14c06be979e1b3332c6c3c254b669a1975
Shinzo SAITO <architshin@websarva.com>
2019-08-28 18:24:05 +0900

    Chapter4用サンプルの追加。 (#2)
    * /helloにPOSTするhtmlファイルの追加。
    
    * Chap4用プロジェクトと最初のサンプル作成。
    
    * 2個目のサンプル追加。
    
    * 3個目のサンプル追加。
    
    * 4個目のサンプル追加。
    
    * 5個目のサンプル追加。
    
    * ルーティング登録の外部ファイル化。
    
    * 6個目のサンプル追加。
    
    * 7個目と8個目のサンプル追加。
    
    * 9個目のサンプル追加。

---
+++ slimroute/routes.php
@@ -0,0 +1,77 @@
+<?php
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+$app->post("/slimroute/public/helloPost",
+	function(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface {
+		print("POSTメソッドでHello World!");
+		return $response;
+	}
+);
+
+$app->post("/slimroute/public/showParams",
+	function(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface {
+		$postParams = $request->getParsedBody();
+		$name = $postParams["name"];
+		$age = $postParams["age"];
+		print("送信されたパラメータ: 名前は".$name."で年齢が".$age);
+		return $response;
+	}
+);
+
+$app->get("/slimroute/public/writeBody",
+	function(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface {
+		$content = "レスポンスボディに文字列を格納";
+		$responseBody = $response->getBody();
+		$responseBody->write($content);
+		return $response;
+	}
+);
+
+$app->any("/slimroute/public/helloAny",
+	function(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface {
+		$method = $request->getMethod();
+		$content = $method."メソッドでHello World!";
+		$responseBody = $response->getBody();
+		$responseBody->write($content);
+		return $response;
+	}
+);
+
+$app->map(["POST", "GET"], "/slimroute/public/helloMap",
+	function(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface {
+		$content = "POSTまたはGETメソッドでHello World!";
+		$responseBody = $response->getBody();
+		$responseBody->write($content);
+		return $response;
+	}
+);
+
+$app->get("/slimroute/public/showDetail/{id}",
+	function(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface {
+		$id = $args["id"];
+		$content = "IDが".$id."の詳細です!";
+		$responseBody = $response->getBody();
+		$responseBody->write($content);
+		return $response;
+	}
+);
+
+$app->redirect("/slimroute/public/google", "https://www.google.com/");
+$app->redirect("/slimroute/public/hey", "/slimroute/public/helloAny", 301);
+
+$app->any("/slimroute/public/redirectOrNot/{flg}",
+	function(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface {
+		$flg = $args["flg"];
+		if($flg == 0) {
+			$response = $response->withHeader("Location", "https://www.google.com/");
+			$response = $response->withStatus(302);
+		}
+		else {
+			$content = "リダイレクトは行いません";
+			$responseBody = $response->getBody();
+			$responseBody->write($content);
+		}
+		return $response;
+	}
+);
