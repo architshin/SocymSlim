e34a927f6fc36f38895cf321ba8cf2537f2c83f8
Shinzo SAITO <architshin@websarva.com>
2019-08-29 15:53:37 +0900

    1コ目のサンプル追加。

---
+++ slimview/routes.php
@@ -0,0 +1,17 @@
+<?php
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+$app->setBasePath("/slimview/public");
+
+$app->any("/getDataByJSON/{id}",
+	function(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface {
+		$id = $args["id"];
+		$jsonArray = ["id" => $id, "name" => "田中和彦", "age" => 26];
+		$jsonData = json_encode($jsonArray);
+		$responseBody = $response->getBody();
+		$responseBody->write($jsonData);
+		$response = $response->withHeader("Content-Type", "application/json");
+		return $response;
+	}
+);
