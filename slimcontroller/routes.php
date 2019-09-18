2a766af7de2646f578d62f2cb4a0c36d629d0092
Shinzo SAITO <architshin@websarva.com>
2019-09-18 17:45:53 +0900

    2コ目のサンプル完成。

--- slimcontroller/routes.php
+++ slimcontroller/routes.php
@@ -1,5 +1,8 @@
 <?php
 use SocymSlim\SlimController\controllers\HelloController;
+use SocymSlim\SlimController\controllers\SeveralMethodsController;
 
 $app->setBasePath("/slimcontroller/public");
 $app->any("/helloWithInvokableController", HelloController::class);
+$app->any("/several/showFirst", SeveralMethodsController::class.":showFirst");
+$app->any("/several/showSecond", SeveralMethodsController::class.":showSecond");
