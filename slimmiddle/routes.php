606e01a150db89509558bf6435b3ac1290d6d7ab
Shinzo SAITO <architshin@websarva.com>
2019-09-27 23:01:36 +0900

    4個目のサンプル追加。

--- slimmiddle/routes.php
+++ slimmiddle/routes.php
@@ -3,8 +3,10 @@ use SocymSlim\SlimMiddle\controllers\SlimMiddleController;
 use SocymSlim\SlimMiddle\middlewares\RecordIPAddress;
 use SocymSlim\SlimMiddle\middlewares\RecordIPAddressBefore;
 use SocymSlim\SlimMiddle\middlewares\BeforeAndAfter;
+use SocymSlim\SlimMiddle\middlewares\Outer;
 
 $app->setBasePath("/slimmiddle/public");
 $app->any("/doRecordIPAddress", SlimMiddleController::class.":middlewareTest")->add(new RecordIPAddress());
 $app->any("/doRecordIPAddressBefore", SlimMiddleController::class.":middlewareTest")->add(new RecordIPAddressBefore());
 $app->any("/beforeAndAfter", SlimMiddleController::class.":middlewareTest")->add(new BeforeAndAfter());
+$app->any("/nested", SlimMiddleController::class.":middlewareTest")->add(new BeforeAndAfter())->add(new Outer());
