42cfbc377a0b7b4a545a2c375d467c0bd98682af
Shinzo SAITO <architshin@websarva.com>
2020-01-19 02:28:14 +0900

    5章DotAccessDataクラスのコーディングスタイルを変更。

--- slimview/DotAccessData.php
+++ slimview/DotAccessData.php
@@ -2,16 +2,20 @@
 class DotAccessData {
 	public $deptName = "経理部";
 	private $csName = "武者小路実篤";
-	public function makeMessage() {
+	public function makeMessage()
+	{
 		return "makeMessageの戻り値です";
 	}
-	public function getCsName() {
+	public function getCsName()
+	{
 		return $this->csName;
 	}
-	public function isFlgOn() {
+	public function isFlgOn()
+	{
 		return true;
 	}
-	public function hasMiddleParam() {
+	public function hasMiddleParam()
+	{
 		return true;
 	}
 }
